bdec82335b806034e27fb8a0b3297d4ac52d05a9
Lianzi <lianziliu.sugar@gmail.com>
2022-12-12 11:01:21 -0500

    commit tree 39b7d373b2522a55fe7c9e4c12927c6af9cb7798

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resource :spices, only[:index, :create, :update, :destroy]
+  get "spices", to: "spices#index"
+  post "spices", to: "spices#create"
+  patch "spices/:id", to: "spices#update"
+  delete "spices/:id", to: "spices#destroy"
 end
